8f3788e5f28dd4b4a3a4b77f3242597bc599de64
Danielle Boczkowski <38707471+DanielleBK@users.noreply.github.com>
2018-06-07 18:21:49 -0300

    Create app.js
    Código JavaScript

---
+++ app.js
@@ -0,0 +1,40 @@
+
+  var values = new Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+  var key = +document.getElementById('key').innerHTML;
+
+    function prev() {
+        if(key > 1) {
+          key--;
+          document.getElementById('key').innerHTML = key;
+       }
+       }
+       function next() {
+           if(key < 25) {
+             key++;
+             document.getElementById('key').innerHTML = key;
+          }
+          }
+       function cipher() {
+           var input = document.getElementById('input').value.toLocaleUpperCase();
+           var result = '';
+               for(var i = 0; i<input.length; i++){ //Passa por cada caracter do input
+                  var abcPosition = input.charCodeAt(i)-64; //Identifica qual letra é do alfabeto
+                  var displacement = (abcPosition + key) % 26; //Faz o deslocamento de César e mantém dentro do alfabeto (26 letras)
+                     displacement = displacement == 0 ? 26 : displacement; //retornar para 0 se resultado seja> 26,
+                     result += values[displacement-1];
+               }
+               // function encipher() {
+               //
+               //     var input = document.getElementById('#input2').value.toLocaleUpperCase();
+               //     var result = '';
+               //         for(var i = 0; i<input.length; i--){
+               //            var abcPosition = input.charCodeAt(i)+64;
+               //            var displacement = (abcPosition - key) % 26;
+               //               displacement = displacement == 0 ? 26 : displacement;
+               //               result += values[displacement-1];
+
+
+
+}
+document.write= result //Plano de Ultima hora
+//document.getElementById('output').innerHTML="result";
