9aeb18255d1576e4f5d1057a98ead5cc67f46011
Danielle Boczkowski <38707471+DanielleBK@users.noreply.github.com>
2018-10-14 17:32:57 -0300

    Troca De Var para Let
    Troca var para let 
    auto indent 
    arrumei o deciframento.

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
+const values = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+let key = +document.getElementById("key").innerHTML;
 
-  var values = new Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-  var key = +document.getElementById('key').innerHTML;
-
-    function prev() {
-        if(key > 1) {
-          key--;
-          document.getElementById('key').innerHTML = key;
-       }
-       }
-       function next() {
-           if(key < 25) {
-             key++;
-             document.getElementById('key').innerHTML = key;
-          }
-          }
-       function cipher() {
-           var input = document.getElementById('input').value.toLocaleUpperCase();
-           var result = '';
-               for(var i = 0; i<input.length; i++){ //Passa por cada caracter do input
-                  var abcPosition = input.charCodeAt(i)-64; //Identifica qual letra é do alfabeto
-                  var displacement = (abcPosition + key) % 26; //Faz o deslocamento de César e mantém dentro do alfabeto (26 letras)
-                     displacement = displacement == 0 ? 26 : displacement; //retornar para 0 se resultado for> 26,
-                     result += values[displacement-1];
-    }
+function prev() {
+  if (key > 1) {
+    key--;
+    document.getElementById("key").innerHTML = key;
   }
-         function encipher() { 
-            var input = document.getElementById('#input2').value.toLocaleUpperCase();
-            var result = '';
-               for(var i = 0; i<input.length; i--){
-                  var abcPosition = input.charCodeAt(i)+64;
-                  var displacement = (abcPosition - key) % 26;
-                    displacement = displacement == 0 ? 26 : displacement;
-                    result += values[displacement-1];
-   }
- }           
-//document.write= result
-document.getElementById('output').innerHTML="result";
+}
+
+function next() {
+  if (key < 25) {
+    key++;
+    document.getElementById("key").innerHTML = key;
+  }
+}
+
+function cipher() {
+  let input = document.getElementById("input").value.toUpperCase();
+  let result = "";
+
+  for (let i = 0; i < input.length; i++) { 
+
+    let abcPosition = input.charCodeAt(i) - 64; 
+    let displacement = (abcPosition + key) % 26; 
+    displacement = displacement == 0 ? 26 : displacement; 
+    result += values[displacement - 1]; 
+  }
+
+  document.getElementById("output").innerHTML = result;
+}
+
+function decripher() {
+  let input = document.getElementById("input2").value.toUpperCase();
+  let result = "";
+
+  for (let i = 0; i < input.length; i++) {
+
+    let abcPosition = input.charCodeAt(i) + 60; 
+    let displacement = (abcPosition - key) % 26;
+    displacement = displacement == 0 ? 26 : displacement; 
+    result += values[displacement - 1]; 
+  }
+
+  document.getElementById("output").innerHTML = result;
+}
